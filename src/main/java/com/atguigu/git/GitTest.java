56a165a85114df956fce8bfb81af511478b87945
IU <957620887@qq.com>
2024-02-21 12:17:27 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author Lu
+ * @create 2024-02-21 12:14
+ */
+public class GitTest {
+
+	public static void main(String[] args) {
+		System.out.println("hello git!");
+	}
+}
